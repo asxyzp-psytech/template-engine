5f0b02d1a7e99155f848de5e1a7c33384ecacccf
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-01-31 15:24:09 +0530

    Exporting all individual model classes

---
+++ Models/index.js
@@ -0,0 +1,9 @@
+const TextTheme = require('./TextTheme');
+const DesignTheme = require('./DesignTheme');
+const TemplateElement = require('./TemplateElement');
+
+module.exports = {
+    text: TextTheme,
+    design: DesignTheme,
+    element: TemplateElement
+};
