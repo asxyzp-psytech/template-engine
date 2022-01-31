3ff993747cee1fb3f5522fe1d10aa6fc67a6f13c
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-01-31 15:24:50 +0530

    Adding Model & templatise function to exports

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
+const Model = require('./Models/index');
 const Import = require('./Methods/Import/index');
 const Validate = require('./Methods/Validate/index');
+const templatise = require('./Methods/templatise');
 
 module.exports = {
+    model: Model,
     import: Import,
-    validate: Validate
+    validate: Validate,
+    templatise: templatise
 };
