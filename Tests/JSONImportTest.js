fd93c822a32a1f0e329bfcc5e23a577d04afdab6
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-01-31 12:08:46 +0530

    Testing JSON import file method

---
+++ Tests/JSONImportTest.js
@@ -0,0 +1,6 @@
+// TESTING IMPORT OF TEMPLATE FILE IN JSON FILE
+const path = require('path');
+const JSON = require('../Methods/Import/json');
+
+const Template = JSON(path.join(__dirname,'template.json'));
+console.log(Template);
