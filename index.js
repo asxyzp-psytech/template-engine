e08d10e91e1d65aa11ceee7f310b213b565396b6
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-01-31 13:56:23 +0530

    exporting object containing various validation methods

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Import = require('./Methods/Import/index');
+const Validate = require('./Methods/Validate/index');
 
 module.exports = {
-    import: Import
+    import: Import,
+    validate: Validate
 };
