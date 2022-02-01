b8d3a2485dd420ded95525611045d7de06d488c9
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-02-01 09:49:47 +0530

    Minor change: Storing data to be templatised in a variable

--- Tests/Templatise/templatise.js
+++ Tests/Templatise/templatise.js
@@ -8,10 +8,19 @@ const templatePath = path.join(__dirname, 'template.json');
 const dataMap = temp.import.dataMap(dataMapPath);
 const template = temp.import.dataMap(templatePath);
 
+// TEMPLATISING DATA ONLY AFTER TEMPLATE TO DATA MAP VALIDATION
 if (temp.validate.mapping(template.data[0], dataMap.data[0])) {
+
+    // DATA TO BE TEMPLATISED
+    const data = { 
+        "title": "ABCD", 
+        "image": "https://pbs.twimg.com/profile_images/1267310089435664384/_JiJQrH__400x400.jpg" 
+    };
+
+    // TEMPLATISED POST CARD
     const templ = temp.templatise(
         template.data[0],
         dataMap.data[0],
-        { "title": "ABCD", "image": "https://pbs.twimg.com/profile_images/1267310089435664384/_JiJQrH__400x400.jpg" }
+        data
     );
 }
