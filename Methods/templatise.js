105a4af4da5aa42299660af364bafff2a0aca9c1
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-02-01 09:47:20 +0530

    Adding additional comments

--- Methods/templatise.js
+++ Methods/templatise.js
@@ -7,17 +7,23 @@
  * @returns {Object} TEMPLATISED DATA
  */
 function templatise(template, dataMap, data) {
+    
     // ITERATING THROUGH EACH ELEMENT IN THE DATA MAP & REPLACING MAPPED DATA IN TEMPLATE
     Object.keys(dataMap.element).forEach(
         (dataMapId) => {
+
             // UNSHIFTING IMAGE SOURCE IN PROPERTIES
             if (template.element[dataMapId].type === "image") {
+                /**data[dataMap.element[dataMapId] => {'children':[...], 'properties': [...], ... etc} */
                 template.element[dataMapId].properties.unshift(`src=${data[dataMap.element[dataMapId].properties]}`);
             }
+            
             // REPLACING TEMPLATE ELEMENT'S CHILD WITH TEXT DATA
             else if (template.element[dataMapId].type === "text") {
+                /**data[dataMap.element[dataMapId] => {'children':[...], 'properties': [...], ... etc} */
                 template.element[dataMapId].children[0] = data[dataMap.element[dataMapId].children];
             }
+            
         }
     );
     return template;
