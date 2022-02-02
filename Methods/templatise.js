40785649c6e104eb171d84dc051b05b3346fc9aa
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-02-02 10:40:54 +0530

    Changing top layout element in the template

--- Methods/templatise.js
+++ Methods/templatise.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid');
+
 /**
  * FUNCTION: templatise(template, dataMap, data)
  * FUNCTIONALITY: TEMPLATISING DATA
@@ -7,7 +9,7 @@
  * @returns {Object} TEMPLATISED DATA
  */
 function templatise(template, dataMap, data) {
-    
+
     // ITERATING THROUGH EACH ELEMENT IN THE DATA MAP & REPLACING MAPPED DATA IN TEMPLATE
     Object.keys(dataMap.element).forEach(
         (dataMapId) => {
@@ -17,15 +19,44 @@ function templatise(template, dataMap, data) {
                 /**data[dataMap.element[dataMapId] => {'children':[...], 'properties': [...], ... etc} */
                 template.element[dataMapId].properties.unshift(`src=${data[dataMap.element[dataMapId].properties]}`);
             }
-            
+
             // REPLACING TEMPLATE ELEMENT'S CHILD WITH TEXT DATA
             else if (template.element[dataMapId].type === "text") {
                 /**data[dataMap.element[dataMapId] => {'children':[...], 'properties': [...], ... etc} */
                 template.element[dataMapId].children[0] = data[dataMap.element[dataMapId].children];
             }
-            
         }
     );
+
+    // STORING CURRENT & NEW TOP ELEMENT ID
+    let oldTopElementId;
+    const newTopElementId = uuid.v4();
+
+    // FINDING THE CURRENT TOP ELEMENT ID
+    Object.keys(template.element).forEach((templateElementId) => {
+        if (template.element[templateElementId].parent === null) {
+            oldTopElementId = templateElementId;
+        }
+    });
+
+    // STORING CURRENT TOP LAYOUT ELEMENT
+    const oldTopElement = template.element[oldTopElementId];
+    oldTopElement.id = newTopElementId;
+
+    // DELETING TOP LAYOUT ELEMENT FROM CURRENT LAYOUT AND REPLACING IT BY NEW TOP LAYOUT ELEMENT
+    template.element[newTopElementId] = oldTopElement;
+    delete (template.element[oldTopElementId]);
+
+    // REPLACING PARENT KEY OF ALL OTHER LAYOUT ELEMENTS WITH NEW TOP ELEMENT ID
+    Object.keys(template.element).forEach((templateElementId) => {
+        if(template.element[templateElementId].parent === oldTopElementId){
+            template.element[templateElementId].parent = newTopElementId;
+        }
+    });
+
+    // REPLACING TEMPLATE'S OLD TOP ELEMENT ID WITH NEW TOP ELEMENT ID
+    template.id = uuid.v4();
+    template.topElementId = newTopElementId;
     return template;
 }
 
