516d3a4abd2f8c1a85e6619fe67469f9b2b6876c
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-02-02 11:23:04 +0530

    Storing temp output in file

--- Tests/Templatise/rssTest.js
+++ Tests/Templatise/rssTest.js
@@ -5,11 +5,13 @@ const path = require('path');
 const temp = require('../../index');
 const Parser = require('rss-parser');
 
-// STORING PATHS OF JSON FILES TO BE IMPORTED
+// STORING SOURCE MAP & TEMPLATE DATA PATHS
 const dataMapPath = path.join(__dirname, 'dataMap.json');
 const templatePath = path.join(__dirname, 'template.json');
+
+// IMPORTING SOURCE MAP & TEMPLATE DATA
 const dataMap = temp.import.dataMap(dataMapPath);
-const template = temp.import.dataMap(templatePath);
+const template = temp.import.template(templatePath);
 
 // FOR STORING TEMPLATISED POST DATA
 const post = {
@@ -37,20 +39,36 @@ async function templatise() {
         const parser = new Parser();
         let feed = await parser.parseURL(feedURL[1]);
 
-        // MAPPING FEED ITEMS TO POST CARDS
-        feed.items.map((data) => {
-            const postCard = { template: temp.templatise(template.data[0], dataMap.data[0], data) };
-            post.data.push(postCard);
-            post.count = post.data.length;
+        // SENDING FEED DATA AS STREAM
+        const feedData = {
+            title: feed.title,
+            description: feed.description,
+            image: feed.image,
+            link: feed.link
+        };
+
+        // ADDING TEMPLATISED FEED DATA TO POST DATA
+        feed.items.forEach((data, index) => {
+            let postCard = {
+                stream: feedData,
+                template: temp.templatise(template.data[0], dataMap.data[0], data)
+            };
+            console.log(postCard.template.id, postCard.template.topElementId, index);
+            post.data = [...post.data, postCard];
         });
+        post.count = post.data.length;
+
+        console.log("\n");
+        post.data.forEach((data, index) => {
+            console.log(data.template.id, data.template.topElementId, index);
+        });
+
+        fs.writeFile(
+            path.join(__dirname, `template-${uuid.v4()}.json`),
+            JSON.stringify(post),
+            (err) => { if (err) throw err; }
+        );
     }
 }
 
-// WRITING TEMPLATISED DATA IN JSON FILE WHICH CAN BE USED BY mutter-render-engine
-templatise().then(() => {
-    fs.writeFile(
-        path.join(__dirname, `template-${uuid.v4()}.json`),
-        JSON.stringify(post),
-        (err) => { if (err) throw err; }
-    );
-});
+templatise();
