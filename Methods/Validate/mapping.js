34616b9c518d46c7d8dff21b7d1df261dff369da
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-02-07 21:34:17 +0530

    + Changing function name from mapping to validateMapping + Simplifying validation process using object iteration using for ... in statement

--- Methods/Validate/mapping.js
+++ Methods/Validate/mapping.js
@@ -1,54 +1,52 @@
+// IMPORTING MODULES/PACKAGES
+const Keys = require('../../keys');
+const requiredElementKeys = Keys.templateElement;
+
 /**
- * FUNCTION: validateMapping(template, dataMap)
- * FUNCTIONALITY: VALIDATES TEMPLATE AND SOURCE DATA MAP
- * @param {object} template TEMPLATE DATA
- * @param {object} dataMap SOURCE DATA MAP
- * @return {true} WHEN TEMPLATE TO SOURCE DATA MAPPING IS VALID
+ * FUNCTION: validateMapping(template, sourceMap)
+ * FUNCTIONALITY: VALIDATES TEMPLATE AND SOURCE SOURCE MAP
+ * @param {object} template TEMPLATE SOURCE
+ * @param {object} sourceMap SOURCE SOURCE MAP
+ * @return {true} WHEN TEMPLATE TO SOURCE SOURCE MAPPING IS VALID
  */
-function mapping(template, dataMap) {
+"use strict";
+function validateMapping(template, sourceMap) {
 
-    // KEYS OF TEMPLATE ELEMENTS
-    const requiredElementKeys = ["id", "parent", "children", "text", "design", "type"];
-
-    // VALIDATING TEMPLATE & DATA MAP DATA TYPE
-    if (!(typeof template === 'object' && typeof dataMap === 'object')) {
-        throw new Error("VALIDATION ERROR: BOTH template & dataMap SHOULD BE OF OBJECT TYPE");
+    // VALIDATING TEMPLATE & SOURCE MAP SOURCE TYPE
+    if (!(typeof template === 'object' && typeof sourceMap === 'object')) {
+        throw new Error("VALIDATION ERROR: BOTH template & sourceMap SHOULD BE OF OBJECT TYPE");
     }
 
     // VALIDATING WHETHER THE SOURCE MAP IS RELEVANT 'FOR' A GIVEN TEMPLATE
-    if (!(template.id === dataMap.for)) {
-        throw new Error(`VALIDATION ERROR: DATA MAP WITH ID ${dataMap.id} IS NOT MAPPED TO TEMPLATE WITH ID ${template.id}`);
+    if (!(template.id === sourceMap.templateId)) {
+        throw new Error(`VALIDATION ERROR: SOURCE MAP WITH ID ${sourceMap.id} IS NOT MAPPED TO TEMPLATE WITH ID ${template.id}`);
     }
 
-    // VALIDATING WHETHER DATA MAP ELEMENT KEY IS A VALID TEMPLATE ELEMENT KEYS
-    const dataMapKeys = Object.keys(dataMap.element);
-    const templateElementKeys = Object.keys(template.element);
-    dataMapKeys.forEach(dataMapKey => {
-        if (!templateElementKeys.includes(dataMapKey)) {
-            throw new Error(`VALIDATION ERROR: DATA MAP WITH ID ${dataMap.id}, CONTAINS AN INVALID TEMPLATE KEY ${dataMapKey}`);
+    // VALIDATING WHETHER SOURCE MAP ELEMENT KEY IS A VALID TEMPLATE ELEMENT KEYS
+    for (const sourceMapElementKey in sourceMap.element) {
+        if (!(sourceMapElementKey in template.element)) {
+            throw new Error(`VALIDATION ERROR: SOURCE MAP WITH ID ${sourceMap.id}, CONTAINS AN INVALID TEMPLATE KEY ${sourceMapKey}`);
         }
-    });
+    }
 
     /* 
-        VALIDATING WHETHER DATA MAP ELEMENTS HAVE APPROPRIATE KEYS OR NOT
+        VALIDATING WHETHER SOURCE MAP ELEMENTS HAVE APPROPRIATE KEYS OR NOT
         e.g.
         {
             id1: { "children": "XYZ" => VALID KEY NAME "children" },
             id2: { "property": "XYZ" => INVALID KEY NAME "property". SHOULD BE "properties" }
         }
     */
-    dataMapKeys.forEach((dataMapKey) => {
-        Object.keys(dataMap.element[dataMapKey]).forEach(
-            (dataMapElementKey) => {
-                if (!requiredElementKeys.includes(dataMapElementKey)) {
-                    throw new Error(`DATA MAP ELEMENT ${dataMapKey} HAS INVALID MAPPING FOR KEY ${dataMapElementKey}`);
-                }
+    for (const sourceMapElementKey in sourceMap.element) {
+        for (const sourceMapElementProperty in sourceMap.element[sourceMapElementKey]) {
+            if (!(sourceMapElementProperty in requiredElementKeys)) {
+                throw new Error(`SOURCE MAP WITH ID ${sourceMap.id} HAS AN ELEMENT WITH ID ${sourceMapElementKey} WHICH HAS INVALID KEY NAME ${sourceMapElementProperty}`);
             }
-        );
-    });
+        }
+    }
 
     // RETURNS true IF NO ERROR IS THROWN
     return true;
 }
 
-module.exports = mapping;
+module.exports = validateMapping;
