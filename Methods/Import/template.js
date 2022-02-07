2a82f4c0c9205a47f3694e4ac117fcfd30b4c733
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-02-07 21:31:55 +0530

    + Changing function name from template to validateTemplate + Simplifying validation for existence of data & count keys using hasOwnProperty()

--- Methods/Import/template.js
+++ Methods/Import/template.js
@@ -7,7 +7,7 @@ const fs = require('fs');
  * @param {String} path: PATH OF THE JSON FILE
  * @returns {Array} Template : ARRAY OF TEMPLATES
  */
-function template(path) {
+function validateTemplate(path) {
 
     //CHECKING FILE TYPE FROM PATH NAME
     if (!path.includes(".json")) {
@@ -20,20 +20,22 @@ function template(path) {
         }
         else {
             // PARSING JSON FILE
-            const JSONData = JSON.parse(fs.readFileSync(path, { encoding: "utf8" }));
-            
+            const templateData = JSON.parse(fs.readFileSync(path, { encoding: "utf8" }));
+
             // VALIDATING BASIC STRUCTURE OF THE JSON TEMPLATE DATA FILE
-            if(!(Object.keys(JSONData).includes("data") && Array.isArray(JSONData.data))){
+            // TEMPLATE SHOULD HAVE A KEY "data" WHOSE VALUE SHOULD BE AN ARRAY
+            if (!(templateData.hasOwnProperty("data") && Array.isArray(templateData.data))) {
                 throw new Error("TYPE ERROR: IMPORTED JSON FILE'S KEY data, SHOULD BE ASSIGNED TO AN ARRAY");
             }
-            if(!(Object.keys(JSONData).includes("count") && typeof JSONData.count === "number")){
+            // TEMPLATE SHOULD HAVE A KEY "count" WHOSE VALUE SHOULD BE A NUMBER
+            if (!(templateData.hasOwnProperty("count") && typeof templateData.count === "number")) {
                 throw new Error("TYPE ERROR: IMPORTED JSON FILE'S KEY count SHOULD BE ASSIGNED TO A NUMBER");
             }
 
             //VALIDATING INDIVIDUAL TEMPLATES #TBD
-            return JSONData;
+            return templateData;
         }
     }
 }
 
-module.exports = template;
+module.exports = validateTemplate;
